dbd5867bf42c9b896c4f868fab4329f291bb59db
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-07-02 23:05:37 +0200

    Added scenario support in flow actions

--- app.js
+++ app.js
@@ -1,10 +1,45 @@
 "use strict";
 
+var taHoma = require('./lib/tahoma');
+
 const Log = require('homey-log').Log;
 
 function init() {
 	
 	Homey.log(`${Homey.manifest.id} running...`);
+
+	//to do: refactor to own file
+	Homey.manager('flow').on('action.activate_scenario.scenario.autocomplete', function(callback, args) {
+		taHoma.getActionGroups(function(err, data) {
+			var scenarios = new Array();
+
+			if (data && data.constructor === Array) {
+				for (var i=0; i<data.length; i++) {
+					var scenario = {
+						oid: data[i].oid,
+						name: data[i].label
+					}
+					scenarios.push(scenario);
+				}
+
+				scenarios = scenarios.filter(function(scenario) {
+					return ( scenario.name.toLowerCase().indexOf( args.query.toLowerCase() ) > -1 );
+				});
+			}
+
+			callback(null, scenarios);
+		});
+	});
+
+	Homey.manager('flow').on('action.activate_scenario', function(callback, args){
+		taHoma.executeScenario(args.scenario.oid, function(err, data) {
+			if (err) {
+				return;
+			}
+
+			callback(null, true);
+		});
+	});
 	
 }
 
