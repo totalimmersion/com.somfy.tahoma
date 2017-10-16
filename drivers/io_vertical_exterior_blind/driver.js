95ca5e651bbc408d2ff700575355fda4b24a4cd2
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-10-16 13:32:23 +0200

    Refactord blind driver to make it compatible with sdk 2

--- drivers/io_vertical_exterior_blind/driver.js
+++ drivers/io_vertical_exterior_blind/driver.js
@@ -98,40 +98,6 @@ class VerticalExteriorBlindDriver extends Driver {
 				}
 			}
 		}
-	}
-
-	onPair(socket) {
-		socket.on('list_devices', function(data, callback) {
-			taHoma.setup(function(err, data) {
-				if (err) {
-					return callback(err);
-				}
-				if (data && data.devices) {
-					var blinds = new Array();
-					for (var i=0; i<data.devices.length; i++) {
-						var device = data.devices[i];
-						if (device.controllableName == 'io:VerticalExteriorAwningIOComponent') {
-							var device_data = {
-								name: device.label,
-								data: {
-									id: device.oid,
-									deviceURL: device.deviceURL,
-									label: device.label
-								}
-							};
-							blinds.push(device_data);
-						}
-					}
-
-					callback(null, blinds);
-				}
-			});
-
-		});
-
-		socket.on('disconnect', function() {
-			console.log("User aborted pairing, or pairing is finished");
-		});
 	}*/
 }
 
