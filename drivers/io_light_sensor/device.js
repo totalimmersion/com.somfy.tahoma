bc0904984e6d76df86032f80bfa814c28fc666c5
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-10-16 10:57:44 +0200

    Created light sensor device class

---
+++ drivers/io_light_sensor/device.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const Homey = require('homey');
+const Driver = require('../../lib/Device');
+const taHoma = require('../../lib/tahoma');
+
+//Device for a io:LightIOSystemSensor device
+class LightSensorDevice extends Device {
+}
+
+module.exports = LightSensorDevice;
