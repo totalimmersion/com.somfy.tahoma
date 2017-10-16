b4141201b5ac51687ae765e9be5e0a971a53c67a
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-10-16 16:10:34 +0200

    Added ‘measure_luminance’ capability to light sensor device

--- drivers/io_light_sensor/device.js
+++ drivers/io_light_sensor/device.js
@@ -11,6 +11,8 @@ class LightSensorDevice extends Device {
 		this.log('device init');
         this.log('name:', this.getName());
         this.log('class:', this.getClass());
+
+        this.registerCapabilityListener('measure_luminance', this.onCapabilityMeasureLuminance.bind(this));
 	}
 
 	onAdded() {
@@ -19,6 +21,64 @@ class LightSensorDevice extends Device {
 
 	onDeleted() {
 		this.log('device deleted');
+	}
+
+	onCapabilityMeasureLuminance(value, opts, callback) {
+		var _this = this;
+		//var deviceData = this.getData();
+
+		var oldLuminance = this.getState().measure_luminance;
+		if (oldLuminance != value) {
+			this.setCapabilityValue('measure_luminance', value);
+			module.exports.realtime(deviceData, 'measure_luminance', value);
+
+			var tokens = {
+				'luminance': value
+			};
+
+			var state  = {
+				'measure_luminance': value
+			}
+
+			//trigger flows
+			new Homey.FlowCardTrigger('change_luminance_more_than')
+				.register()
+				.trigger(tokens, state)
+				.catch(this.error)
+				.then(this.log);
+
+			new Homey.FlowCardTrigger('change_luminance_less_than')
+				.register()
+				.trigger(tokens, state)
+				.catch(this.error)
+				.then(this.log);
+
+			new Homey.FlowCardTrigger('change_luminance_between')
+				.register()
+				.trigger(tokens, state)
+				.catch(this.error)
+				.then(this.log);
+
+			/*Homey.manager('flow').triggerDevice('change_luminance_more_than', tokens, state, device_data, function(err, result) {
+				if (err) {
+					return Homey.error(err);
+				}
+			});
+
+			Homey.manager('flow').triggerDevice('change_luminance_less_than', tokens, state, device_data, function(err, result) {
+				if (err) {
+					return Homey.error(err);
+				}
+			});
+
+			Homey.manager('flow').triggerDevice('change_luminance_between', tokens, state, device_data, function(err, result) {
+				if (err) {
+					return Homey.error(err);
+				}
+			});*/
+		}
+
+		callback(null, value);
 	}
 }
 
