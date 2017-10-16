1ad8937907fd8363fea4890e9145c32b0fb12ae2
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-10-16 13:35:51 +0200

    Added onInit, onAdded and onDeleted listeners to temperature sensor device

--- drivers/io_temperature_sensor/device.js
+++ drivers/io_temperature_sensor/device.js
@@ -6,6 +6,20 @@ const taHoma = require('../../lib/tahoma');
 
 //Device for a io:TemperatureIOSystemSensor device
 class TemperatureSensorDevice extends Device {
+
+	onInit() {
+		this.log('device init');
+        this.log('name:', this.getName());
+        this.log('class:', this.getClass());
+	}
+
+	onAdded() {
+		this.log('device added');
+	}
+
+	onDeleted() {
+		this.log('device deleted');
+	}
 }
 
 module.exports = TemperatureSensorDevice;
