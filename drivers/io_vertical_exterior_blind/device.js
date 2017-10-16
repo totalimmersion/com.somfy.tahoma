7368a8be51c1707b3c59fa52e6619c0a770ddc76
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-10-16 10:58:11 +0200

    Created blind device class

---
+++ drivers/io_vertical_exterior_blind/device.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const Homey = require('homey');
+const Driver = require('../../lib/Device');
+const taHoma = require('../../lib/tahoma');
+
+//Device for a io:VerticalExteriorAwningIOComponent device
+class VerticalExteriorBlindDevice extends Device {
+}
+
+module.exports = VerticalExteriorBlindDevice;
