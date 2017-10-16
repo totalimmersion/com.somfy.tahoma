7c9b4b8f47bc83c1bf6b0d1ee09339200bd1aa3c
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-10-16 10:58:23 +0200

    Created temperature sensor device class

---
+++ drivers/io_temperature_sensor/device.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const Homey = require('homey');
+const Driver = require('../../lib/Device');
+const taHoma = require('../../lib/tahoma');
+
+//Device for a io:TemperatureIOSystemSensor device
+class TemperatureSensorDevice extends Device {
+}
+
+module.exports = TemperatureSensorDevice;
