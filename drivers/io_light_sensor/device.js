79a198ff0602cdb0e26b020c30aa5506cffaf5a3
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-10-16 13:35:20 +0200

    Added onInit, onAdded and onDeleted listeners to light sensor device

--- drivers/io_light_sensor/device.js
+++ drivers/io_light_sensor/device.js
@@ -6,6 +6,20 @@ const taHoma = require('../../lib/tahoma');
 
 //Device for a io:LightIOSystemSensor device
 class LightSensorDevice extends Device {
+
+	onInit() {
+		this.log('device init');
+        this.log('name:', this.getName());
+        this.log('class:', this.getClass());
+	}
+
+	onAdded() {
+		this.log('device added');
+	}
+
+	onDeleted() {
+		this.log('device deleted');
+	}
 }
 
 module.exports = LightSensorDevice;
