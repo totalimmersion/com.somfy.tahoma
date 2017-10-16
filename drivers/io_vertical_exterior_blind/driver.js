98e76fdd3b1484045e89d84472aa0bd5d87a410c
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-10-16 21:00:42 +0200

    Moved flow listeners to driver

--- drivers/io_vertical_exterior_blind/driver.js
+++ drivers/io_vertical_exterior_blind/driver.js
@@ -13,6 +13,47 @@ var windowcoveringsStateMap = {
 //Driver for a io:VerticalExteriorAwningIOComponent device
 class VerticalExteriorBlindDriver extends Driver {
 
+	onInit() {
+		/*** ADD FLOW ACTION LISTENERS ***/
+		new Homey.FlowCardAction('activate_scenario')
+			.register()
+			.registerRunListener((args, state) => {
+
+				taHoma.executeScenario(args.scenario.oid, function(err, data) {
+					if (err) {
+						return;
+					}
+
+					return Promise.resolve(null, true);
+				});
+
+			})
+			.getArgument('scenario')
+			.registerAutocompleteListener((query, args) => {
+
+				taHoma.getActionGroups(function(err, data) {
+					var scenarios = new Array();
+
+					if (data && data.constructor === Array) {
+						for (var i=0; i<data.length; i++) {
+							var scenario = {
+								oid: data[i].oid,
+								name: data[i].label
+							}
+							scenarios.push(scenario);
+						}
+
+						scenarios = scenarios.filter(function(scenario) {
+							return ( scenario.name.toLowerCase().indexOf( query.toLowerCase() ) > -1 );
+						});
+					}
+
+					return Promise.resolve(scenarios);
+				});
+
+			});
+	}
+
 	_onPairListDevices(state, data, callback) {
 
 		taHoma.setup(function(err, data) {
@@ -41,64 +82,6 @@ class VerticalExteriorBlindDriver extends Driver {
 		});
 
 	}
-
-	/*constructor() {
-		super();
-		var _self = this;
-
-		this.capabilities = {
-			windowcoverings_state: {
-				get: function(device_data, callback) {
-					try {
-						var device = _self.getDeviceByData(device_data);
-						callback(null, device.state.windowcoverings_state);
-					} catch(e) {
-						console.log(e.message);
-						callback(e);
-					}
-				},
-
-				set: function(device_data, state, callback, fromCloudSync) {
-					var device = _self.getDeviceByData(device_data);
-					if(device instanceof Error) {
-						return callback(device);
-					}
-
-					var oldWindowCoveringsState = device.state.windowcoverings_state;
-					if (oldWindowCoveringsState != state) {
-						if (state == 'idle' && device.executionId) {
-							taHoma.cancelExecution(device.executionId, function(err, result) {
-								if (!err) {
-									//let's set the state to open, because Tahoma, doesn't have an idle state. If a blind isn't closed for 100%, the state will remain open.
-									device.state.windowcoverings_state = state;
-									//module.exports.realtime(device_data, 'windowcoverings_state', device.state.windowcoverings_state);
-									callback(null, device.state.windowcoverings_state);
-								}
-							});
-						} else if(!(oldWindowCoveringsState == 'idle' && fromCloudSync == true)) {
-							var action = {
-								name: windowcoveringsStateMap[state],
-								parameters: []
-							};
-
-							if (!fromCloudSync) {
-								taHoma.executeDeviceAction(device_data.label, device_data.deviceURL, action, function(err, result) {
-									if (!err) {
-										device.executionId = result.execId;
-										device.state.windowcoverings_state = state;
-										module.exports.realtime(device_data, 'windowcoverings_state', device.state.windowcoverings_state);
-										callback(null, state);
-									}
-								});
-							} else {
-								device.state.windowcoverings_state = state;
-							}
-						}
-					}
-				}
-			}
-		}
-	}*/
 }
 
 module.exports = VerticalExteriorBlindDriver;
