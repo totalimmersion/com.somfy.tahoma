722feec08745828095251bfde8f34afe571edce8
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-10-16 21:05:54 +0200

    Moved luminance triggers and conditions to driver

--- drivers/io_light_sensor/driver.js
+++ drivers/io_light_sensor/driver.js
@@ -7,6 +7,52 @@ const taHoma = require('../../lib/tahoma');
 //Driver for a io:LightIOSystemSensor device
 class LightSensorDriver extends Driver {
 
+	onInit() {
+		/*** LUMINANCE TRIGGERS ***/
+		new Homey.FlowCardTrigger('change_luminance_more_than')
+			.register()
+			.registerRunListener((args, state) => {
+				let conditionMet = state.measure_luminance < args.luminance;
+				return Promise.resolve(conditionMet);
+			});
+
+		new Homey.FlowCardTrigger('change_luminance_less_than')
+			.register()
+			.registerRunListener((args, state) => {
+				let conditionMet = state.measure_luminance < args.luminance;
+				return Promise.resolve(conditionMet);
+			});
+
+		new Homey.FlowCardTrigger('change_luminance_between')
+			.register()
+			.registerRunListener((args, state) => {
+				let conditionMet = state.measure_luminance > args.luminance_from && state.measure_luminance < args.luminance_to;
+				return Promise.resolve(conditionMet);
+			});
+
+		/*** LUMINANCE CONDITIONS ***/
+		new Homey.FlowCardCondition('has_luminance_more_than')
+			.register()
+			.registerRunListener((args, state) => {
+				let conditionMet = state.measure_luminance > args.luminance;
+				return Promise.resolve(conditionMet);
+			});
+
+		new Homey.FlowCardCondition('has_luminance_less_than')
+			.register()
+			.registerRunListener((args, state) => {
+				let conditionMet = state.measure_luminance < args.luminance;
+				return Promise.resolve(conditionMet);
+			});
+
+		new Homey.FlowCardCondition('has_luminance_between')
+			.register()
+			.registerRunListener((args, state) => {
+				let conditionMet = state.measure_luminance > args.luminance_from && state.measure_luminance < args.luminance_to;
+				return Promise.resolve(conditionMet);
+			});
+	}
+
 	_onPairListDevices(state, data, callback) {
 
 		taHoma.setup(function(err, data) {
