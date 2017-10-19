757895aefd398d9c848fcffd03ffa6efd67709ec
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-10-19 23:27:07 +0200

    Moved flow action listeners to app initialization

--- app.js
+++ app.js
@@ -8,7 +8,47 @@ class App extends Homey.App {
 	onInit() {		
 		this.log(`${Homey.app.manifest.id} running...`);
 
+		this.addScenarioActionListeners();
 		syncManager.init();
+	}
+
+	addScenarioActionListeners() {
+		/*** ADD FLOW ACTION LISTENERS ***/
+		new Homey.FlowCardAction('activate_scenario')
+			.register()
+			.registerRunListener((args, state) => {
+				return taHoma.executeScenario(args.scenario.oid)
+					.then(data => {
+						return Promise.resolve();
+					});
+			})
+			.getArgument('scenario')
+			.registerAutocompleteListener((query, args) => {
+				return taHoma.getActionGroups()
+					.then(function (data) {
+						var scenarios = new Array();
+
+						if (data && data.constructor === Array) {
+							for (var i=0; i<data.length; i++) {
+								var scenario = {
+									oid: data[i].oid,
+									name: data[i].label
+								};
+								scenarios.push(scenario);
+							}
+
+							scenarios = scenarios.filter(function(scenario) {
+								return ( scenario.name.toLowerCase().indexOf( query.toLowerCase() ) > -1 );
+							});
+
+						}
+
+						return Promise.resolve(scenarios);
+					})
+					.catch(function (error) {
+						console.log(error.message);
+					});
+			});
 	}
 }
 
--- drivers/io_vertical_exterior_blind/driver.js
+++ drivers/io_vertical_exterior_blind/driver.js
@@ -13,45 +13,6 @@ var windowcoveringsStateMap = {
 //Driver for a io:VerticalExteriorAwningIOComponent device
 class VerticalExteriorBlindDriver extends Driver {
 
-	onInit() {
-		/*** ADD FLOW ACTION LISTENERS ***/
-		new Homey.FlowCardAction('activate_scenario')
-			.register()
-			.registerRunListener((args, state) => {
-				return taHoma.executeScenario(args.scenario.oid)
-					.then(data => {
-						return Promise.resolve(true);
-					});
-			})
-			.getArgument('scenario')
-			.registerAutocompleteListener((query, args) => {
-				return taHoma.getActionGroups()
-					.then(function (data) {
-						var scenarios = new Array();
-
-						if (data && data.constructor === Array) {
-							for (var i=0; i<data.length; i++) {
-								var scenario = {
-									oid: data[i].oid,
-									name: data[i].label
-								};
-								scenarios.push(scenario);
-							}
-
-							scenarios = scenarios.filter(function(scenario) {
-								return ( scenario.name.toLowerCase().indexOf( query.toLowerCase() ) > -1 );
-							});
-
-						}
-
-						return Promise.resolve(scenarios);
-					})
-					.catch(function (error) {
-						console.log(error.message);
-					});
-			});
-	}
-
 	_onPairListDevices(state, data, callback) {
 
 		taHoma.setup(function(err, data) {
