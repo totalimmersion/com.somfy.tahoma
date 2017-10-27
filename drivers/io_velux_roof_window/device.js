b0c6a384353a2d6c3d693a492bfadc0f163c34a2
Lennart Kuijs <lennart@pinkcube-werbeartikel.de>
2017-10-27 13:35:16 +0200

    RoofWindowDevice inherits from WindowCoveringsDevice now

--- drivers/io_velux_roof_window/device.js
+++ drivers/io_velux_roof_window/device.js
@@ -1,61 +1,11 @@
 "use strict";
 
 const Homey = require('homey');
-const Device = require('../../lib/Device');
-const Tahoma = require('../../lib/Tahoma');
-
-const windowcoveringsStateMap = {
-	up: 'open',
-	idle: null,
-	down: 'close'
-};
+const WindowCoveringsDevice = require('../../lib/WindowCoveringsDevice');
 
 //Device for a io:WindowOpenerVeluxIOComponent device
-class RoofWindowDevice extends Device {
+class RoofWindowDevice extends WindowCoveringsDevice {
 
-	onInit() {
-		super.onInit();
-
-        this.registerCapabilityListener('windowcoverings_state', this.onCapabilityWindowcoveringsState.bind(this));
-	}
-
-	onCapabilityWindowcoveringsState(value, opts, callback) {
-		var _this = this;
-		var deviceData = this.getData();
-		var oldWindowCoveringsState = this.getState().windowcoverings_state;
-		if (oldWindowCoveringsState != value) {
-			if (value == 'idle' && this.getStoreValue('executionId')) {
-				Tahoma.cancelExecution(this.getStoreValue('executionId'), function(err, result) {
-					if (!err) {
-						//let's set the state to open, because Tahoma, doesn't have an idle state. If a blind isn't closed for 100%, the state will remain open.
-						_this.setCapabilityValue('windowcoverings_state', value);
-						if (callback) {
-							callback(null, value);
-						}
-					}
-				});
-			} else if(!(oldWindowCoveringsState == 'idle' && opts.fromCloudSync == true)) {
-				var action = {
-					name: windowcoveringsStateMap[value],
-					parameters: []
-				};
-
-				if (!opts.fromCloudSync) {
-					Tahoma.executeDeviceAction(deviceData.label, deviceData.deviceURL, action, function(err, result) {
-						if (!err) {
-							_this.setStoreValue('executionId', result.execId);
-							_this.setCapabilityValue('windowcoverings_state', value);
-							if (callback) {
-								callback(null, value);
-							}
-						}
-					});
-				} else {
-					_this.setCapabilityValue('windowcoverings_state', value);
-				}
-			}
-		}
-	}
 }
 
 module.exports = RoofWindowDevice;
