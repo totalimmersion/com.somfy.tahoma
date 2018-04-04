04981b2e5aaeda75c0b784d3244725a6111c6dad
Lennart Kuijs <mail@lennartkuijs.nl>
2018-04-04 16:05:09 +0200

    Added unit tests for all the devices

---
+++ drivers/io_horizontal_awning/device.spec.js
@@ -0,0 +1,14 @@
+const expect = require('chai').expect;
+const mock = require('mock-require');
+
+const Homey = require('../../lib/test/homey-mock');
+mock('homey', Homey);
+
+const Device = require('./device');
+
+describe('HorizontalAwningDevice', function() {
+	const device = new Device();
+	it('should implement the sync() method', function() {
+		expect(() => device.sync([])).to.not.throw();
+	});
+});
