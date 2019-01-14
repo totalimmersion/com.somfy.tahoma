07ca975e0f51045c35eef653f925a88d620e5320
Lennart Kuijs <mail@lennartkuijs.nl>
2019-01-14 17:16:07 +0100

    Added support for VELUX IO exterior roller shutters

--- drivers/io_roller_shutter/driver.js
+++ drivers/io_roller_shutter/driver.js
@@ -9,7 +9,7 @@ const Driver = require('../Driver');
 class RollerShutterDriver extends Driver {
 
   onInit() {
-    this.deviceType = ['io:RollerShutterGenericIOComponent', 'io:RollerShutterWithLowSpeedManagementIOComponent'];
+    this.deviceType = ['io:RollerShutterGenericIOComponent', 'io:RollerShutterWithLowSpeedManagementIOComponent', 'io:RollerShutterVeluxIOComponent'];
   }
 
 }
