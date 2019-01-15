1e1f22d2ea904b1d1b67f39b3650344168fc61e7
Lennart Kuijs <mail@lennartkuijs.nl>
2019-01-15 11:26:28 +0100

    Fixed linting issues

--- api.js
+++ api.js
@@ -4,38 +4,38 @@ const Homey = require('homey');
 const Tahoma = require('./lib/Tahoma');
 
 module.exports = [
-	{
-		description: 'Authenticate TaHoma',
-		method: 'POST',
-		path: '/login/',
-		fn: function (args, callback) {
-			Tahoma.login(args.body.username, args.body.password)
-			.then(result => {
-				Homey.ManagerSettings.set('username', args.body.username);
-				Homey.ManagerSettings.set('password', args.body.password);
-				callback(null, result);
-			})
-			.catch(error => {
-				console.log(error.message, error.stack);
-				callback(error);
-			});
-		}
-	},
-	{
-		description: 'Log out of TaHoma',
-		method: 'POST',
-		path: '/logout/',
-		fn: function (args, callback) {
-			Tahoma.logout()
-			.then(result => {
-				Homey.ManagerSettings.unset('username');
-				Homey.ManagerSettings.unset('password');
-				callback(null, result);
-			})
-			.catch(error => {
-				console.log(error.message, error.stack);
-				callback(error);
-			});
-		}
-	}
+  {
+    description: 'Authenticate TaHoma',
+    method: 'POST',
+    path: '/login/',
+    fn: function (args, callback) {
+      Tahoma.login(args.body.username, args.body.password)
+        .then(result => {
+          Homey.ManagerSettings.set('username', args.body.username);
+          Homey.ManagerSettings.set('password', args.body.password);
+          callback(null, result);
+        })
+        .catch(error => {
+          console.log(error.message, error.stack);
+          callback(error);
+        });
+    }
+  },
+  {
+    description: 'Log out of TaHoma',
+    method: 'POST',
+    path: '/logout/',
+    fn: function (args, callback) {
+      Tahoma.logout()
+        .then(result => {
+          Homey.ManagerSettings.unset('username');
+          Homey.ManagerSettings.unset('password');
+          callback(null, result);
+        })
+        .catch(error => {
+          console.log(error.message, error.stack);
+          callback(error);
+        });
+    }
+  }
 ];
--- app.js
+++ app.js
@@ -13,67 +13,67 @@ const INITIAL_SYNC_INTERVAL = 10; //interval of 10 seconds
  **/
 class App extends Homey.App {
 
-	/**
+  /**
 	 * Initializes the app
 	 */
-	onInit() {
-		this.log(`${Homey.app.manifest.id} running...`);
-		this.addScenarioActionListeners();
+  onInit() {
+    this.log(`${Homey.app.manifest.id} running...`);
+    this.addScenarioActionListeners();
 
-		if (!Homey.ManagerSettings.get('syncInterval')) {
-			Homey.ManagerSettings.set('syncInterval', INITIAL_SYNC_INTERVAL);
-		}
+    if (!Homey.ManagerSettings.get('syncInterval')) {
+      Homey.ManagerSettings.set('syncInterval', INITIAL_SYNC_INTERVAL);
+    }
 
-		Homey.ManagerSettings.on('set', (setting) => {
-			if (setting === 'syncInterval') this.initSync();
-		});
-		Homey.on('settings.set', this.initSync);
+    Homey.ManagerSettings.on('set', (setting) => {
+      if (setting === 'syncInterval') this.initSync();
+    });
+    Homey.on('settings.set', this.initSync);
 
-		this.initSync();
-	}
+    this.initSync();
+  }
 
-	/**
+  /**
 	 * Initializes synchronization between Homey and TaHoma
 	 * with the interval as defined in the settings.
 	 */
-	initSync() {
-		let interval = null;
-		try {
-			interval = Number(Homey.ManagerSettings.get('syncInterval'));
-		} catch(e) {
-			interval = INITIAL_SYNC_INTERVAL;
-		}
-		syncManager.init(interval * 1000);
-	}
+  initSync() {
+    let interval = null;
+    try {
+      interval = Number(Homey.ManagerSettings.get('syncInterval'));
+    } catch(e) {
+      interval = INITIAL_SYNC_INTERVAL;
+    }
+    syncManager.init(interval * 1000);
+  }
 
-	/**
+  /**
 	 * Adds a listener for flowcard scenario actions
 	 */
-	addScenarioActionListeners() {
-		/*** ADD FLOW ACTION LISTENERS ***/
-		new Homey.FlowCardAction('activate_scenario')
-			.register()
-			.registerRunListener(args => {
-				return Tahoma.executeScenario(args.scenario.oid)
-					.then(() => {
-						return Promise.resolve();
-					});
-			})
-			.getArgument('scenario')
-			.registerAutocompleteListener(query => {
-				return Tahoma.getActionGroups()
-					.then(data => {
-						const scenarios = data
-											.map(({ oid, label }) => ({ oid, name: label }))
-											.filter(({ name }) => name.toLowerCase().indexOf(query.toLowerCase()) > -1);
+  addScenarioActionListeners() {
+    /*** ADD FLOW ACTION LISTENERS ***/
+    new Homey.FlowCardAction('activate_scenario')
+      .register()
+      .registerRunListener(args => {
+        return Tahoma.executeScenario(args.scenario.oid)
+          .then(() => {
+            return Promise.resolve();
+          });
+      })
+      .getArgument('scenario')
+      .registerAutocompleteListener(query => {
+        return Tahoma.getActionGroups()
+          .then(data => {
+            const scenarios = data
+              .map(({ oid, label }) => ({ oid, name: label }))
+              .filter(({ name }) => name.toLowerCase().indexOf(query.toLowerCase()) > -1);
 
-						return Promise.resolve(scenarios);
-					})
-					.catch(error => {
-						console.log(error.message, error.stack);
-					});
-			});
-	}
+            return Promise.resolve(scenarios);
+          })
+          .catch(error => {
+            console.log(error.message, error.stack);
+          });
+      });
+  }
 }
 
 module.exports = App;
