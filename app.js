e62c301428dce40dbffe2e02f7dc34c8a79d85f7
AdyRock <38184396+AdyRock@users.noreply.github.com>
2020-08-27 22:07:11 +0100

    Added My button. Convert Up/Down to quite.

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 if ( process.env.DEBUG === '1' )
 {
-    require( 'inspector' ).open( 9222, '0.0.0.0' )
+    require( 'inspector' ).open( 9222, '0.0.0.0', true )
 }
 
 const Homey = require( 'homey' );
--- drivers/io_roller_shutter_quiet/device.js
+++ drivers/io_roller_shutter_quiet/device.js
@@ -1,45 +1,43 @@
 'use strict';
 
-const WindowCoveringsDevice = require( '../WindowCoveringsDevice' );
+const WindowCoveringsDevice = require('../WindowCoveringsDevice');
 
 /**
  * Device class for roller shutters with the io:RollerShutterWithLowSpeedManagementIOComponent controllable name in TaHoma
  * @extends {WindowCoveringsDevice}
  */
-class RollerShutterDeviceQuiet extends WindowCoveringsDevice
-{
-    onInit()
-    {
+class RollerShutterDeviceQuiet extends WindowCoveringsDevice {
+    onInit() {
         super.onInit();
 
-        if (!this.hasCapability( 'my_position' ))
-        {
-            this.addCapability( 'my_position' );
+        if (!this.hasCapability('my_position')) {
+            this.addCapability('my_position');
         }
 
-        this.registerCapabilityListener( 'quiet_mode', this.onCapabilityQuietMode.bind( this ) );
+        this.registerCapabilityListener('quiet_mode', this.onCapabilityQuietMode.bind(this));
 
-        this.quietMode = this.getCapabilityValue( 'quiet_mode' );
-        if ( this.quietMode )
-        {
+        this.quietMode = this.getCapabilityValue('quiet_mode');
+        if (this.quietMode) {
             this.setPositionActionName = 'setPositionAndLinearSpeed';
-        }
-        else
-        {
+        } else {
             this.setPositionActionName = 'setClosure';
         }
     }
 
-    async onCapabilityQuietMode( value, opts )
-    {
+    async onCapabilityQuietMode(value, opts) {
         this.quietMode = value;
-        if ( value )
-        {
+        if (value) {
             this.setPositionActionName = 'setPositionAndLinearSpeed';
-        }
-        else
-        {
+        } else {
             this.setPositionActionName = 'setClosure';
+        }
+    }
+
+    async onCapabilityWindowcoveringsState(value, opts) {
+        if (!opts.fromCloudSync && (this.setPositionActionName === 'setPositionAndLinearSpeed') && ((value === 'up') || (value === 'down'))) {
+            super.onCapabilityWindowcoveringsSet(value === 'up' ? 0 : 1, opts);
+        } else {
+            super.onCapabilityWindowcoveringsState(value, opts);
         }
     }
 }
