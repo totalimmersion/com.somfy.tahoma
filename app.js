629879a29cd5c9798489c3a3bebce60828255fe8
AdyRock <38184396+AdyRock@users.noreply.github.com>
2020-09-07 01:23:53 +0100

    remove mail preview

--- app.js
+++ app.js
@@ -124,17 +124,13 @@ class myApp extends Homey.App {
                 });
 
                 // send mail with defined transport object
-                let info = await transporter.sendMail({
+                await transporter.sendMail({
                     from: '"Homey User" <' + Homey.env.MAIL_USER + '>', // sender address
                     to: Homey.env.MAIL_RECIPIENT, // list of receivers
                     subject: subject, // Subject line
                     text: text // plain text body
                 });
 
-                // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-                // Preview only available when sending through an Ethereal account
-                console.log("Preview URL: ", nodemailer.getTestMessageUrl(info));
                 return {error: null, message: "OK"};
             } catch (err) {
                 this.logError("Send log error", err);
