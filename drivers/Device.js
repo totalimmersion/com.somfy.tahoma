5c453c270fe9dc6f87bf55cf64695264faf2d321
AdyRock <38184396+AdyRock@users.noreply.github.com>
2021-02-05 11:04:20 +0000

    Added mor atlanta devices

--- drivers/Device.js
+++ drivers/Device.js
@@ -14,10 +14,12 @@ class Device extends Homey.Device
         this.log('Device init:', this.getName(), 'class:', this.getClass());
         this.sync();
     }
+
     onAdded()
     {
         this.log('device added');
     }
+
     onDeleted()
     {
         if (this.timerId)
@@ -26,6 +28,7 @@ class Device extends Homey.Device
         }
         this.log('device deleted');
     }
+
     /**
      * Returns the TaHoma device url
      * @return {String}
@@ -34,6 +37,7 @@ class Device extends Homey.Device
     {
         return this.getData().deviceURL;
     }
+
     /**
      * Returns the io controllable name(s) of TaHoma
      * @return {Array} deviceType
@@ -42,10 +46,175 @@ class Device extends Homey.Device
     {
         return this.getDriver().getDeviceType();
     }
+
     isReady()
     {
         return this._ready;
     }
+
+    async onCapability(capabilityXRef, value, opts)
+    {
+        if (!opts || !opts.fromCloudSync)
+        {
+            let somfyValue = value;
+            if (capabilityXRef.scale)
+            {
+                somfyValue *= capabilityXRef.scale;
+            }
+
+            const deviceData = this.getData();
+            var action = {
+                name: capabilityXRef.somfyNameSet,
+                parameters: [somfyValue]
+            };
+
+            let result = await Tahoma.executeDeviceAction(deviceData.label, deviceData.deviceURL, action);
+            if (result !== undefined)
+            {
+                if (result.errorCode)
+                {
+                    Homey.app.logInformation(this.getName(),
+                    {
+                        message: result.error,
+                        stack: result.errorCode
+                    });
+
+                    if (this.boostSync)
+                    {
+                        await Homey.app.unBoostSync();
+                    }
+                    throw (new Error(result.error));
+                }
+                else
+                {
+                    this.executionId = result.execId;
+                }
+            }
+            else
+            {
+                Homey.app.logInformation(this.getName() + ": onCapability " + capabilityXRef.somfyNameSet, "Failed to send command");
+                if (this.boostSync)
+                {
+                    await Homey.app.unBoostSync();
+                }
+                throw (new Error("Failed to send command"));
+            }
+        }
+        else
+        {
+            try
+            {
+                await this.setCapabilityValue(capabilityXRef.homeyName, value);
+            }
+            catch (err)
+            {
+                Homey.app.logInformation(this.getName() + ": onCapability " + capabilityXRef.homeyName, err);
+            }
+        }
+    }
+
+    /**
+     * Gets the sensor data from the TaHoma cloud
+     * Capabilities{somfyNameGet: 'name of the Somfy capability, homeyName: 'name of the Homey capability', compare: 'text for true value or not specified if real value' }
+     */
+    async syncList(Capabilities)
+    {
+        try
+        {
+            const states = await this.getSync();
+            if (states)
+            {
+                // Look for each of the required capabilities
+                for (const capability of Capabilities)
+                {
+                    try
+                    {
+                        const state = states.find(state => state.name === capability.somfyNameGet);
+                        if (state)
+                        {
+                            // Found the entry
+                            Homey.app.logStates(this.getName() + ": " + capability.somfyNameGet + "= " + state.value);
+                            await this.triggerCapabilityListener(capability.homeyName, (capability.compare ? (state.value === capability.compare) : (capability.scale ? state.value / capability.scale : state.value)),
+                            {
+                                fromCloudSync: true
+                            });
+                        }
+                    }
+                    catch (error)
+                    {
+                        Homey.app.logInformation(this.getName(),
+                        {
+                            message: error.message,
+                            stack: error.stack
+                        });
+                    }
+                }
+            }
+        }
+        catch (error)
+        {
+            Homey.app.logInformation(this.getName(),
+            {
+                message: error.message,
+                stack: error.stack
+            });
+        }
+    }
+
+    // look for updates in the events array
+    async syncEventsList(events, Capabilities)
+    {
+        if (events === null)
+        {
+            return this.getSync();
+        }
+
+        const myURL = this.getDeviceUrl();
+
+        // Process events sequentially so they are in the correct order
+        const oldStates = this.getState();
+
+        // For each event that has been received
+        for (var i = 0; i < events.length; i++)
+        {
+            // get the event
+            const element = events[i];
+
+            // Ensure we are processing a state changed event
+            if (element['name'] === 'DeviceStateChangedEvent')
+            {
+                // If the URL matches the it is for this device
+                if ((element['deviceURL'] === myURL) && element['deviceStates'])
+                {
+                    // Got what we need to update the device so lets process each capability
+                    for (var x = 0; x < element.deviceStates.length; x++)
+                    {
+                        // Get the Somfy capability
+                        const deviceState = element.deviceStates[x];
+
+                        // look up the entry so we can get the Homey capability, etc
+                        const found = Capabilities.find(element => element.somfyNameGet === deviceState.name);
+
+                        if (found)
+                        {
+                            // Yep we can relate to this one
+                            Homey.app.logStates(this.getName() + ": " + found.somfyNameGet + "= " + deviceState.value);
+                            const oldState = oldStates[found.homeyName];
+                            const newState = (found.compare ? (deviceState.value === found.compare) : (deviceState.value));
+                            if (oldState !== newState)
+                            {
+                                this.triggerCapabilityListener(found.homeyName, newState,
+                                {
+                                    fromCloudSync: true
+                                });
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
     async sync()
     {
         try
@@ -62,7 +231,7 @@ class Device extends Homey.Device
                 {
                     return await Tahoma.getDeviceStates(deviceURL);
                 }
-                
+
                 if (process.env.DEBUG === '1')
                 {
                     const simData = Homey.ManagerSettings.get('simData');
---
+++ drivers/io_atlantic_apc_zone_controller/device.js
@@ -0,0 +1,75 @@
+'use strict';
+
+const SensorDevice = require('../SensorDevice');
+const Tahoma = require('../../lib/Tahoma');
+const Homey = require('homey');
+
+/**
+ * Device class for the opening detector with the io:AtlanticDomesticHotWaterProductionV2_AEX_IOComponent controllable name in TaHoma
+ * @extends {SensorDevice}
+ */
+
+/**
+  List of capabilities to look for
+ * CapabilitiesXRef {
+        somfyNameGet: 'name of the Somfy capability to get the value',
+        somfyNameSet: 'name of the Somfy capability to set the value',
+        homeyName: 'name of the Homey capability',
+        compare: 'text for true value or not specified if real value',
+        scale: scale factor
+    }
+*/
+const CapabilitiesXRef = [
+    { somfyNameGet: '', somfyNameSet: 'cancelAbsence', homeyName: 'cancel_absence' },
+    { somfyNameGet: 'core:ErrorCodeState', somfyNameSet: '', homeyName: 'error_code_state' },
+    { somfyNameGet: 'core:HeatingDerogationAvailabilityState', somfyNameSet: '', homeyName: 'heating_derogation_availability_state' },
+    { somfyNameGet: 'io:LastPassAPCOperatingModeState', somfyNameSet: '', homeyName: 'last_pass_apc_operating_mode_state' },
+    { somfyNameGet: 'io:PassAPCProductTypeState', somfyNameSet: '', homeyName: 'pass_apc_product_type_state' },
+    { somfyNameGet: 'io:ThermalSchedulingModeState', somfyNameSet: '', homeyName: 'thermal_scheduling_mode_state' },
+    { somfyNameGet: 'core:AbsenceCoolingTargetTemperatureState', somfyNameSet: 'setAbsenceCoolingTargetTemperature', homeyName: 'target_temperature.absence_cooling' },
+    { somfyNameGet: 'core:AbsenceHeatingTargetTemperatureState', somfyNameSet: 'setAbsenceHeatingTargetTemperature', homeyName: 'target_temperature.absence_heating' },
+    { somfyNameGet: 'core:HeatingCoolingAutoSwitchState', somfyNameSet: 'setHeatingCoolingAutoSwitch', homeyName: 'heating_cooling_auto_switch', compare: 'on'},
+    { somfyNameGet: 'io:PassAPCOperatingModeState', somfyNameSet: 'setPassAPCOperatingMode', homeyName: 'pass_apc_operating_mode' },
+];
+
+class AtlanticZoneControllerDevice extends SensorDevice
+{
+    async onInit()
+    {
+        this.boostSync = true;
+
+        CapabilitiesXRef.forEach(element =>
+        {
+            this.registerCapabilityListener(element.homeyName, this.onCapability.bind(this, element));
+        });
+
+
+        await super.onInit();
+    }
+
+    /**
+     * Gets the sensor data from the TaHoma cloud
+     */
+    async sync()
+    {
+        super.syncList(CapabilitiesXRef);
+    }
+
+    async getSync()
+    {
+        return super.sync();
+    }
+
+    // look for updates in the events array
+    async syncEvents(events)
+    {
+        if (events === null)
+        {
+            return this.sync();
+        }
+
+        this.syncEventsList(CapabilitiesXRef);
+    }
+}
+
+module.exports = AtlanticZoneControllerDevice;
