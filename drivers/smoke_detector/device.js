044b3d659317b09f5830b3dc8b862afd8b6338de
AdyRock <38184396+AdyRock@users.noreply.github.com>
2021-03-29 18:41:44 +0100

    Fixes

--- drivers/smoke_detector/device.js
+++ drivers/smoke_detector/device.js
@@ -5,6 +5,9 @@ const SensorDevice = require('../SensorDevice');
 
 const CapabilitiesXRef = [
     { homeyName: 'alarm_smoke', somfyNameGet: 'core:SmokeState', somfyNameSet: '', compare: ['', 'detected'] },
+    { homeyName: 'alarm_battery.radio', somfyNameGet: 'io:MaintenanceRadioPartBatteryState', somfyNameSet: '', compare: ['normal', 'low'] },
+    { homeyName: 'alarm_battery.sensor', somfyNameGet: 'io:MaintenanceSensorPartBatteryState', somfyNameSet: '', compare: ['normal', 'low'] },
+    { homeyName: 'alarm_room_state', somfyNameGet: 'io:SensorRoomState', somfyNameSet: '', compare: ['0', '1'] },
     { homeyName: 'test_smoke', somfyNameGet: '', somfyNameSet: 'checkEventTrigger', parameters: 'short' }
 ];
 class SmokeDetectorDevice extends SensorDevice
@@ -15,6 +18,21 @@ class SmokeDetectorDevice extends SensorDevice
         if (!this.hasCapability('test_smoke'))
         {
             this.addCapability('test_smoke');
+        }
+
+        if (!this.hasCapability('alarm_battery.radio'))
+        {
+            this.addCapability('alarm_battery.radio');
+        }
+
+        if (!this.hasCapability('alarm_battery.sensor'))
+        {
+            this.addCapability('alarm_battery.sensor');
+        }
+
+        if (!this.hasCapability('alarm_room_state'))
+        {
+            this.addCapability('alarm_room_state');
         }
 
         await super.onInit(CapabilitiesXRef);
