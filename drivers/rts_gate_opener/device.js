ed53c3329d1b230a2885246d9e834bb197298b4d
AdyRock <38184396+AdyRock@users.noreply.github.com>
2021-04-24 22:08:10 +0100

    Added RTS Gate Opener

---
+++ drivers/rts_gate_opener/device.js
@@ -0,0 +1,145 @@
+/*jslint node: true */
+'use strict';
+
+const Device = require('../Device');
+const Tahoma = require('../../lib/Tahoma');
+const Homey = require('homey');
+
+class rtsGateOpenerDevice extends Device
+{
+    async onInit()
+    {
+        await super.onInit();
+        this.registerCapabilityListener('open_button', this.onCapabilityOpen.bind(this));
+        this.registerCapabilityListener('close_button', this.onCapabilityClose.bind(this));
+        this.registerCapabilityListener('stop_button', this.onCapabilityStop.bind(this));
+    }
+
+    async onCapabilityOpen(value)
+    {
+        if (this.commandExecuting === 'open')
+        {
+            // This command is still processing
+            return;
+        }
+
+        return this.sendOpenCloseStop('open');
+    }
+
+    async onCapabilityClose(value)
+    {
+        if (this.commandExecuting === 'close')
+        {
+            // This command is still processing
+            return;
+        }
+
+        return this.sendOpenCloseStop('close');
+    }
+
+    async onCapabilityStop(value)
+    {
+        if (this.commandExecuting === 'stop')
+        {
+            // This command is still processing
+            return;
+        }
+
+        return this.sendOpenCloseStop('stop');
+    }
+
+    async sendOpenCloseStop(value)
+    {
+        if (this.boostSync)
+        {
+            await Homey.app.boostSync();
+        }
+
+        const deviceData = this.getData();
+        if (this.executionId !== null)
+        {
+            // Wait for previous command to complete
+            let retries = 20;
+            while ((this.executionId !== null) && (retries-- > 0))
+            {
+                await Homey.app.asyncDealy(500);
+            }
+        }
+
+        var action;
+        action = {
+            name: value,
+            parameters: [this.getSetting('option')]
+        };
+
+        let result = await Tahoma.executeDeviceAction(deviceData.label, deviceData.deviceURL, action);
+        if (result !== undefined)
+        {
+            if (result.errorCode)
+            {
+                Homey.app.logInformation(this.getName(),
+                {
+                    message: result.error,
+                    stack: result.errorCode
+                });
+
+                if (this.boostSync)
+                {
+                    await Homey.app.unBoostSync();
+                }
+                throw (new Error(result.error));
+            }
+            else
+            {
+                this.commandExecuting = action.name;
+                this.executionId = result.execId;
+            }
+        }
+        else
+        {
+            Homey.app.logInformation(this.getName() + ": sendOpenClose", "Failed to send command");
+            if (this.boostSync)
+            {
+                await Homey.app.unBoostSync();
+            }
+            throw (new Error("Failed to send command"));
+        }
+    }
+
+    // look for updates in the events array
+    async syncEvents(events)
+    {
+        if (events === null)
+        {
+            return this.sync();
+        }
+
+        const myURL = this.getDeviceUrl();
+
+        // Process events sequentially so they are in the correct order
+        for (var i = 0; i < events.length; i++)
+        {
+            const element = events[i];
+            if (element.name === 'ExecutionStateChangedEvent')
+            {
+                if ((element.newState === 'COMPLETED') || (element.newState === 'FAILED'))
+                {
+                    if (this.executionId === element.execId)
+                    {
+                        if (this.boostSync)
+                        {
+                            await Homey.app.unBoostSync();
+                        }
+                        this.commandExecuting = '';
+                        this.executionId = null;
+                    }
+                }
+            }
+        }
+
+        return myURL;
+    }
+
+}
+
+module.exports = rtsGateOpenerDevice;
